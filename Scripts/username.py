1a8858b5ac5b481513f24c68cafd9b3e961cf590
idontcaro <40004943+idontcaro@users.noreply.github.com>
2019-02-16 17:25:59 -0600

    Create username.py
    Create a program that asks user to input full name and age, then print message to user telling them the year they turn 100.

---
+++ Scripts/username.py
@@ -0,0 +1,6 @@
+#Python exercise 1: create program that asks user to input their name and age
+#exercise 2: print message to user telling year they turn 100
+name = input("give name: ")
+print("Your name is: " + name)
+
+read_lines()
